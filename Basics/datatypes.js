b3a0b8c50d9b457f2886815be0630a0be59f0cb1
Sandip Roy <sroy914434@gmail.com>
2025-04-24 16:48:55 +0000

    all about datatypes part 1

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "sandip"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+//bigint
+//string =""
+//boolen = true /false
+//null => standalone vlaue
+// undefined =>
+// symbol => unique
+
+//object
