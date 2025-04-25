e2d039d76f465df21fa4df13f1b3c9d806a330e1
Sandip Roy <sroy914434@gmail.com>
2025-04-25 05:58:30 +0000

    datatypes part 2

---
+++ Basics/datatypes2.js
@@ -0,0 +1,30 @@
+// premitive
+
+// 7 types -
+// String, Number , Boolean , null , undefined , Symbol, BigInt
+const score = 200
+const scoreValue = 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+//this twos are different Symbol
+const id = Symbol('234')
+const anotherId = Symbol('234')
+// const bigNumber = 123456789012345n
+
+
+
+// Reference (non premitive)
+// Array, Object,functions
+
+const heros = ["ironman","thor"]
+let myObj=
+{
+    mame:"sandip",
+    age:22
+}
+
+const myFunction = function (){
+    console.log("sandip");
+}
