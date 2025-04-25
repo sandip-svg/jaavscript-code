84c6487bd0b2af66cabbc2c3fea5834eae7d346c
Sandip Roy <sroy914434@gmail.com>
2025-04-25 05:13:15 +0000

    add postfix prefix

--- Basics/stringToNumberConversion.js
+++ Basics/stringToNumberConversion.js
@@ -1,8 +1,20 @@
-console.log("1" + 2)
-console.log(1 + "2")
-console.log("1" + "2")
-console.log("1" + 2 + 2)
-console.log(1 + 2 + "2")
-console.log(1 + 2 + "2" +2)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(1 + 2 + "2" +2)
 
-console.log(+true)
+// console.log(+true)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
