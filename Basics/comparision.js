d997d2121325a123b6c6ab55ca8911b2d7afe9ce
Sandip Roy <sroy914434@gmail.com>
2025-04-25 05:33:29 +0000

    about comparosion

---
+++ Basics/comparision.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1) // js automatically convert string to number; igonre this type of code
+
+console.log(null > 0) 
+console.log(null == 0) 
+console.log(null >= 0)
+
+//comparision convert null to a  number treating it as 0
+// equal to not treating as 0
+// thats why (null >= 0) is true and (null == 0) is false
+// undefined gives false for all above equation
+// avoide all above type code 
+
+// === checks the value with its datatypes strictly
