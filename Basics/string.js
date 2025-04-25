b2a7f250a249fc3cd21f4711fcbd10b1c7646203
Sandip Roy <sroy914434@gmail.com>
2025-04-25 07:24:02 +0000

    read string using mdn and  browser

---
+++ Basics/string.js
@@ -0,0 +1,14 @@
+const name = " sandip"
+const newString = new String ("Sandip Roy")
+const repoCount = 50
+
+// console.log(name + repoCount)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// console.log(newString.length)
+// console.log(newString.toUpperCase())
+// console.log(newString.charAt(5))
+// console.log(newString.indexOf('a'))
+console.log(newString.anchor("sandip"))
+
